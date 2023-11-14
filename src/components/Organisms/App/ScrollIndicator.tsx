ac626c40e19d7d2ce24849e5f9ccded38cd745a8
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-11-14 16:21:15 +0100

    Add header scroll indicator

---
+++ src/components/Organisms/App/ScrollIndicator.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import styles from './ScrollIndicator.module.css';
+
+const ScrollIndicator = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const onScroll = () => {
+    const winScroll = document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollTop(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.progressMainStyle}
+        style={{ width: `${scrollTop}%`, height: !scrollTop ? '0px' : '1px' }}
+      ></div>
+    </div>
+  );
+};
+
+export default ScrollIndicator;
