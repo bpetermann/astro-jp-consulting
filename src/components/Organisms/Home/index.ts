600765c230eb3bb06a2d9593c4ef736462b5d2a6
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-11-15 10:04:01 +0100

    Add procedure section

--- src/components/Organisms/Home/index.ts
+++ src/components/Organisms/Home/index.ts
@@ -1,4 +1,5 @@
 export { default as About } from './About.astro';
 export { default as BoxTiles } from './BoxTiles.astro';
 export { default as CardList } from './CardList.astro';
+export { default as Procedure } from './Procedure.astro';
 export { default as TextTile } from './TextTile.astro';
